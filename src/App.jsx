70feeca8cde4e1c38537400bbbee3885ed404029
httpArbaz <128707115+httpArbaz@users.noreply.github.com>
2023-10-07 21:13:54 +0500

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import Home from "./pages/HomePage/home";
+import Home from "../src/pages/HomePage/Home";
 import Profile from "./components/Profile/Profile";
-import Login from "./pages/Login/Login";
+import Login from "../src/pages/Login/Login";
 
 function App() {
   const [Submit, setSubmit] = useState(false);
